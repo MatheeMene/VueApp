864a4396eea0e4099dbbbf9c2297e58a3b14e2e5
Matheus Menegat <matheus.menegat@compasso.com.br>
2019-10-24 17:20:01 -0300

    Get data from local api and return

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,11 +2,36 @@
   <div class="principal-content">
     <h1 class="title-home">Começando com <span class="color-vue">VueJS</span></h1>
     <hr>
+    <div>
+      <ul class="list-itens" style="list-style-type: none;">
+        <li v-bind:key="link.id" v-for="link of links">
+          <img class="img-responsive" :src="link.image" :alt="link.title">
+          <h3 class="image-title"> {{ link.title }} </h3>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+  export default {
 
+    data () {
+
+      return {
+
+        links: []
+        
+      }
+    },
+    created() {
+      
+      this.$http.get("http://localhost:3000/data")
+        .then(res => res.json())
+        .then(res => this.links = res);
+      
+    }
+  }
 </script>
 
 <style>
@@ -19,6 +44,7 @@
   .principal-content {
     margin-left: 23%;
     height: 100vh;
+    padding-left: 2%;
   }
   .title-home {
     text-align: center;
@@ -34,5 +60,10 @@
     height: 1px;
     width: 80%;
     background-image: linear-gradient(to right, rgba(169, 169, 169, 0), rgba(169, 169, 169, .75), rgba(169, 169, 169, 0));
+    margin-bottom: 2%;
   }
+  
+
+ 
+  
 </style>
--- src/components/shared/headerfooter/Header.vue
+++ src/components/shared/headerfooter/Header.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-
+  
 </script>
 
 <style>
